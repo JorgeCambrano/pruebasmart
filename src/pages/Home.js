bae887c66761ffe79c2a6b98e774866382a9c682
JorgeCambrano <JorgeJesusMC@hotmail.com>
2020-02-27 01:42:16 -0600

    prueba

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+class Home extends Component {
+    render() {
+    return (
+
+        <div>
+           SMARTFIELD
+
+            
+        
+        </div>
+    );
+    }
+}
+
+
+export default Home;
