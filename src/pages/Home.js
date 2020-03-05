1fc187a99e8b537d0bf571598af1de6cca81e91c
JorgeCambrano <JorgeJesusMC@hotmail.com>
2020-03-05 01:36:24 -0600

    nuevo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,20 +5,20 @@ class Home extends Component {
     return (
 
       <div >
-        
+
         <div class="row">
           <div class="mx-auto text-primary"><h1>  SMARTH FIELD<span class="badge badge-secondary"> New</span></h1></div>
           <div class="jumbotron">
-                        <h1>“La mejor inversión en tu negocio es con nosotros”.</h1>
-                        <p class="text-justify"> Venttas y compras de terrenos, con gestion de control de manera sistematizado en informacion
+            <h1>“La mejor inversión en tu negocio es con nosotros”.</h1>
+            <p class="text-justify"> Venttas y compras de terrenos, con gestion de control de manera sistematizado en informacion
                         de detallada del terreno, mostraccion de precios, visualizacion de ubicacion y urbanizaccion.</p>
-                        <p><a href="#" class="btn btn-success btn-lg btn-block">Registrate con nosotros </a></p>
-                    
-                </div>
+            <p><a href="#" class="btn btn-success btn-lg btn-block">Registrate con nosotros </a></p>
+
+          </div>
         </div>
-      
+
       </div>
-      
+
     );
   }
 }
