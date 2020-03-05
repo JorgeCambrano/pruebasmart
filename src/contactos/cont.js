3eaf4ee2f9a43898e3445f9f29b00843eeefe87f
danielarcosalvaro <daniealvaro2000@gmail.com>
2020-03-05 11:02:15 -0600

    imagen listo

--- src/contactos/cont.js
+++ src/contactos/cont.js
@@ -8,37 +8,41 @@ class cont extends Component {
             <div>
                 <hr></hr>
                 <div class="container">
-                <div class="row"> 
+                    <div class="row">
 
-                <div class="col-sm-4">
-                    <img src={Imag1} class="img-thumbnail" alt="Cinque Terre" width="150px" height="150px" margin="auto"/>
+                        <div class="col-sm-4">
+                            <img src={Imag1} class="img-thumbnail" alt="Cinque Terre" width="150px" height="150px" margin="auto" display="inline-block" />
 
-                        <div class="card-body ">
-                            <p class="card-text">DANIEL ARCOS ALVARO</p>
-                            <p>Ingeniero en sistemas computacionales</p>
-                            <p>ENCARGADO DE LA EMPRESA</p>
-                            <p>FACEBOOK: dani AB Alvarado </p>
-                            <p>WhatsApp: +52 9161116705</p>
+                            <div class="card-body ">
+                                <p class="card-text">DANIEL ARCOS ALVARO</p>
+                                <p>Ingeniero en sistemas computacionales</p>
+                                <p>ENCARGADO DE LA EMPRESA</p>
+                                <p>FACEBOOK: dani AB Alvarado </p>
+                                <p>WhatsApp: +52 9161116705</p>
+                            </div>
                         </div>
-                
+                        <div className="col-sm-4">
+                            <div class="container-fluid bg-3 text-center">
+                                <img src={Imag2} class="img-thumbnail " alt="Cinque Terre" width="150px" height="150px" paddig="250px" />
 
-                    <div class="container-fluid bg-3 text-center">
-                    <img src={Imag2} class="img-thumbnail " alt="Cinque Terre" width="150px" height="150px" paddig="250px"/>
+                                <div class="card-body">
+                                    <p class="card-text">JORGE JESUS MENDEZ CAMBRANO </p>
+                                    <p>Ingeniero en sistemas computacionales </p>
+                                    <p>ENCARGADO DE LA EMPRESA </p>
+                                    <p>FACEBOOK: Jorge J Cambrano </p>
+                                    <p>WhatsApp: +52 9161317186.</p>
+                                </div>
+                            </div>
 
-                        <div class="card-body">
-                            <p class="card-text">JORGE JESUS MENDEZ CAMBRANO </p>
-                            <p>Ingeniero en sistemas computacionales </p>
-                            <p>ENCARGADO DE LA EMPRESA </p>
-                            <p>FACEBOOK: Jorge J Cambrano </p>
-                            <p>WhatsApp: +52 9161317186.</p>
+
+
+
                         </div>
+                    </div>
                 </div>
-                </div>
-</div>
-</div>
-                </div>
-                );
-            }
-        }
-        
+            </div>
+        );
+    }
+}
+
 export default cont;
