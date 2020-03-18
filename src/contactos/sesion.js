3d08d0cc757541599989f2355838d0951aa65775
danielarcosalvaro <daniealvaro2000@gmail.com>
2020-03-18 17:57:06 -0600

    actual cambio bro

--- src/contactos/sesion.js
+++ src/contactos/sesion.js
@@ -2,46 +2,49 @@ import React, { Component } from 'react';
 import './forma.css';
 
 class sesion extends Component {
- 
+
     render() {
         return (
-            <div> 
-                
-                <div className="container">
-                    <div class="abs-center"></div>
-                <h5 >Iniciar sesion</h5>
-                    <div >
-                    
-                        <forma action="#" className="border  forma"/>
-                        
-                            <div className="forma-group">
-                            
-                                <label for="email">Correo usuario</label>
-                                <input type="text" className="form-control" id="correo" placeholder="Ingrese su correo" name="correo" />
-                           
+            <div className="row">
+                    <div className="col-lg-12">
+                        <br/>
+                        <div class="card cardf|">
+                            <br/>
+                            <br/>
+                        <h5 class="card-header">Iniciar Sesion</h5>
+                            <div class="card-body">                                                           
+
                                 
-                             </div>
-                             
-                                <div className="form-group">
-                                
-                                    <label form="password">Contraseña</label>
-                                    <input type="text" className="form-control" id="passWord" placeholder="Ingrese su contraseña" name="passWord" />
-                                    
-                                    
-                                </div>
-                                <div className="form-group">
-                                <input type="submit" value="Iniciar" className="btn btn-primary"/>
-                                </div>
+                                    <div clas="container" />
+                                    <div class="formaf">
+                                        <br/>
+
+                                        <form action="#" class="  forma" />
+                                        
+                                        <div class="form-group">
+                                            <label for="apellidos">Correo</label>
+                                            <input type="text" className="form-control" id="correo" placeholder="Ingrese su correo" name="correo" />
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="correo">Correo</label>
+                                            <input type="text" className="form-control" id="contraseña" placeholder="Ingrese su contraseña" name="contraseña" />
+                                        </div>
+                                        
+                                       
+
+                                        <input type="submit" value="Iniciar" className="btn btn-primary" />
+                                    </div>
                                
-                                
-                                
-                       </div>
-                       
+
+                            </div>
+                        </div>
                     </div>
-            </div>
-                            
-        );
-               }
-            }
+
+                </div>
             
+
+        );
+    }
+}
+
 export default sesion;
